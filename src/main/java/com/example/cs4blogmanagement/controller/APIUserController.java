7421b7cbf277982c49a4fbc1ef02c6adb0976891
Thaistranf <anhnamduong9x9@gmail.com>
2023-11-03 15:29:26 +0700

    xay dung class APIUserController trong package controller

---
+++ src/main/java/com/example/cs4blogmanagement/controller/APIUserController.java
@@ -0,0 +1,12 @@
+package com.example.cs4blogmanagement.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin("*")
+public class APIUserController {
+
+
+
+}
