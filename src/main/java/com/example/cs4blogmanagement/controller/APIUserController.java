1393c1f173db44d0bf3f439651c04138da36b528
Thaistranf <anhnamduong9x9@gmail.com>
2023-11-03 15:45:47 +0700

    khai bao @Autowired userRepository

--- src/main/java/com/example/cs4blogmanagement/controller/APIUserController.java
+++ src/main/java/com/example/cs4blogmanagement/controller/APIUserController.java
@@ -1,6 +1,7 @@
 package com.example.cs4blogmanagement.controller;
 
 import com.example.cs4blogmanagement.model.User;
+import com.example.cs4blogmanagement.repository.IUserRepository;
 import com.example.cs4blogmanagement.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,9 @@ import org.springframework.web.bind.annotation.*;
 public class APIUserController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private IUserRepository userRepository;
 
     @PostMapping("/register")
     public ResponseEntity<Void> register(@RequestBody User user){
