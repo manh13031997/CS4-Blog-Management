0bbdea92909fab7b4253b9a1013e0eee2140a01a
Thaistranf <anhnamduong9x9@gmail.com>
2023-11-03 15:41:23 +0700

    xay dung chuc nang register

--- src/main/java/com/example/cs4blogmanagement/controller/APIUserController.java
+++ src/main/java/com/example/cs4blogmanagement/controller/APIUserController.java
@@ -1,7 +1,10 @@
 package com.example.cs4blogmanagement.controller;
 
+import com.example.cs4blogmanagement.model.User;
 import com.example.cs4blogmanagement.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,9 +14,10 @@ public class APIUserController {
     @Autowired
     private UserService userService;
 
-
-
-
-
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@RequestBody User user){
+        userService.save(user);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
