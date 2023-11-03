af8e35b543e5887e0c7057f17a435fef2f4f4ccd
Thaistranf <anhnamduong9x9@gmail.com>
2023-11-03 15:36:53 +0700

    khai bao @Autowired userService

--- src/main/java/com/example/cs4blogmanagement/controller/APIUserController.java
+++ src/main/java/com/example/cs4blogmanagement/controller/APIUserController.java
@@ -1,11 +1,18 @@
 package com.example.cs4blogmanagement.controller;
 
+import com.example.cs4blogmanagement.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 @CrossOrigin("*")
 public class APIUserController {
+    @Autowired
+    private UserService userService;
+
+
+
 
 
 
