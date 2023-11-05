7b3b82aa1041688dbc6b1698e1e21e6c8eb5b438
Thaistranf <anhnamduong9x9@gmail.com>
2023-11-05 09:43:13 +0700

    fix chuc nang hien thi alert khi nguoi dung nhap sai format age + email

--- src/main/resources/templates/loginForm/js/function.js
+++ src/main/resources/templates/loginForm/js/function.js
@@ -1,5 +1,5 @@
  function login(){
-    axios.post("http://localhost:8080/users/login", {
+    axios.post("http://localhost:8082/users/login", {
         username: document.getElementById("usn").value,
         password: document.getElementById("pas").value
     }).then(response => {
@@ -18,7 +18,7 @@
      let email = document.getElementById("email").value;
      let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-     axios.post("http://localhost:8080/users/register", {
+     axios.post("http://localhost:8082/users/register", {
          username: document.getElementById("usn").value,
          age: document.getElementById("age").value,
          address: document.getElementById("address").value,
@@ -28,6 +28,9 @@
          if (regex.test(email) && parseInt(age) >= 1) {
              alert("Register successful!!!")
              redirectToLoginForm("http://localhost:63342/CS4-Blog-Management/src/main/resources/templates/loginForm/loginForm.html?_ijt=shuescjtqi2g4iagmfd3pq5h4r&_ij_reload=RELOAD_ON_SAVE")
+         } else if (parseInt(age) < 1 && regex.test(email) === false) {
+             alert("Enter age greater than or equal to 1 and invalid email address!")
+             reloadPage()
          } else if (parseInt(age) < 1) {
              alert("Enter age greater than or equal to 1")
              reloadPage()
